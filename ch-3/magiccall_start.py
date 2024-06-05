bccc577fa81052160bc14241b0190ddf4a9bd2b5
Jason Hargrove <rijaha@gmail.com>
2024-06-05 16:18:24 -0500

    Corrected Files

--- ch-3/magiccall_start.py
+++ ch-3/magiccall_start.py
@@ -9,24 +9,13 @@ class Book:
         self.author = author
         self.price = price
 
-    # The __str__ function is used to return a user-friendly string
-    # representation of the object
     def __str__(self):
         return f"{self.title} by {self.author}, costs {self.price}"
 
-    # The __str__ function is used to return a developer-friendly string
-    # representation of the object
-    def __repr__(self):
-        return f"title={self.title},author={self.author},price={self.price}"
+    # TODO: the __call__ method can be used to call the object like a function
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
-# print each object
-print(b1)
-print(b2)
-
-# use str() and repr()
-print(str(b1))
-print(repr(b2))
+# TODO: call the object as if it were a function
--- ch-3/magicstr_start.py
+++ ch-3/magicstr_start.py
@@ -9,20 +9,13 @@ class Book:
         self.author = author
         self.price = price
 
-    def __str__(self):
-        return f"{self.title} by {self.author}, costs {self.price}"
+    # TODO: use the __str__ method to return a string
 
-    # TODO: the __call__ method can be used to call the object like a function
-    def __call__(self, title, author, price):
-        self.title = title
-        self.author = author
-        self.price = price
+    # TODO: use the __repr__ method to return an obj representation
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
-# call the object as if it were a function
 print(b1)
-b1("Anna Karenina", "Leo Tolstoy", 49.95)
-print(b1)
+print(b2)
